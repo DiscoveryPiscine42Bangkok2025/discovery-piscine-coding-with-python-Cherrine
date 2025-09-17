5dcb8301e99cd6ab963e90e5b0b8acecef37fe14
Cherrine <66070031@kmitl.ac.th>
2025-09-17 23:18:10 +0700

    Chess board Setup

--- rush/checkmate.py
+++ rush/checkmate.py
@@ -22,11 +22,23 @@ def checkmate(board):
         return
 
     # demo function(is_king_in_check)
+    # if you wanted to use this please uncomment, thank you
 
     # if is_king_in_check(grid, king_pos, [0], king_pos[1], board_size):
     #     print('Success')
     # else:
     #     print('Fail')
 
-def find_king(grid)
+def find_king(grid, board_size):
+    """Find and validate King Position"""
+    king_pos = None
+    for i in range(board_size):
+        for j in range(board_size):
+            if grid[i][j] == 'K':
+                if king_pos is not None:
+                    return None
+                
+                king_pos = (i, j)
+
+    return king_pos
     
