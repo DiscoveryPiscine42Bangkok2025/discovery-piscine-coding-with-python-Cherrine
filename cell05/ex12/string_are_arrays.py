7da4fe94151863cf309bb5f8919beb458314915f
Cherrine <66070031@kmitl.ac.th>
2025-09-19 00:46:03 +0700

    Fix the code

--- cell05/ex12/string_are_arrays.py
+++ cell05/ex12/string_are_arrays.py
@@ -1,10 +1,10 @@
 def main(text):
     char_array = list(text.lower())
-    count = 0
+    count = ''
     
     for i in range(len(char_array)):
         if char_array[i] == 'z':
-            count += 1
+            count += 'z'
     print(count)
 
 main(input())
